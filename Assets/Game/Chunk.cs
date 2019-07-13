532cd271665e65361836d42227eaab93443d6e72
Futatsuki W. Shina <thirteenth.electrum@gmail.com>
2019-07-14 06:19:34 +0900

    Changed chunk generation. Now all chunks is within where camera displays

---
+++ Assets/Game/Chunk.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Playable
+{
+    public class Chunk
+    {
+
+    }
+}
