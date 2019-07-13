a18484e985a05385c6bdea9cc1f430d82ac7c720
Futatsuki W. Shina <thirteenth.electrum@gmail.com>
2019-07-14 03:19:54 +0900

    Implemented file separator

--- Assets/Game/FileSeparator.cs
+++ Assets/Game/FileSeparator.cs
@@ -12,6 +12,7 @@ namespace Game.Preprocessor
     public class FileSeparator
     {
         private string _raw;
+        private string _encoding;
         private Dictionary<int, string> _chunkedFile;
 
         /// <summary>
@@ -21,5 +22,29 @@ namespace Game.Preprocessor
         {
             _raw = System.IO.File.ReadAllText(filePath);
         }
+
+        /// <summary>
+        /// ファイルを指定された単位で分割する
+        /// <param name="unit">分割する単位文字数</param>
+        /// </summary>
+        public Dictionary<int, string> ChunkFiles(int unit)
+        {
+            unit = (_raw.Length < unit) ? _raw.Length : unit;
+            if(unit == 0) { return null; }
+
+            var results = new Dictionary<int, string>();
+
+            for(int i = 0; i < _raw.Length; i++)
+            {
+                var start = i * unit;
+                var end   = (i + 1) * unit;
+
+                if(end > _raw.Length) { break; }
+
+                results.Add(i, _raw.Substring(start, unit));
+            }
+
+            return results;
+        } 
     }
 }
---
+++ Assets/Game/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void Start()
+        {
+            // .metaファイルを除いたソースコードを列挙する
+            var directories = new List<string>(
+                System.IO.Directory.GetFiles(Application.streamingAssetsPath + "/SourcecodeFiles/")
+            );
+
+            var targets = directories.Where(str => str.IndexOf(".meta", StringComparison.Ordinal) < 0).ToList();
+
+            // 列挙されたコードの中から一つを選ぶ
+            var index   = UnityEngine.Random.Range(0, targets.Count);
+        }
+    }
+}
